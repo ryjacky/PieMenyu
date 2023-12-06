749bbd5a2345cb419d5814b00dfe2ff2eea5a5d8
ryjacky <ryjacky@outlook.com>
2023-12-06 13:51:03 +0900

    chore: PieMenuEditorPage layout

--- lib/ui/routes/pieMenuEditorPage/pie_menu_properties.dart
+++ lib/ui/routes/pieMenuEditorPage/pie_menu_properties.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:localization/localization.dart';
 
 class PieMenuProperties extends StatefulWidget {
   const PieMenuProperties({super.key});
@@ -10,6 +11,25 @@ class PieMenuProperties extends StatefulWidget {
 class _PieMenuPropertiesState extends State<PieMenuProperties> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DefaultTabController(
+        length: 2,
+        child: Column(
+          children: [
+            TabBar(
+              tabs: [
+                Tab(text: "tab-properties".i18n()),
+                Tab(text: "tab-actions".i18n()),
+              ],
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  const Placeholder(),
+                  const Placeholder(),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
