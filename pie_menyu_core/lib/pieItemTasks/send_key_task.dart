902c0bdee80e54676be6e1e3e309342836f15915
ryjacky <ryjacky@outlook.com>
2023-12-25 16:14:48 +0900

    chore: release all pressed key before execution

--- pie_menyu_core/lib/pieItemTasks/send_key_task.dart
+++ pie_menyu_core/lib/pieItemTasks/send_key_task.dart
@@ -1,7 +1,10 @@
 library pie_menyu_core;
 
+import 'package:ffi/ffi.dart';
+import 'dart:ffi';
 import 'package:flutter_auto_gui/flutter_auto_gui.dart';
 import 'package:pie_menyu_core/db/pie_item_task.dart';
+import 'package:win32/win32.dart';
 
 import '../executor/executable.dart';
 
@@ -61,15 +64,37 @@ class SendKeyTask extends PieItemTask with Executable {
 
   @override
   Future<void> execute() async {
+    // Get the state of all keys
+    final keyboardState = calloc<BYTE>(256);
+    GetKeyboardState(keyboardState);
+
+    // Release all keys that are currently pressed
+    for (var i = 0; i < 256; i++) {
+      final keyState = GetAsyncKeyState(i);
+
+      // Release the key if it is currently being pressed
+      if ((keyState & 0x8000) != 0) {
+        final inputs = calloc<INPUT>(1);
+        inputs[0].type = INPUT_KEYBOARD;
+        inputs[0].ki.wVk = i;
+        inputs[0].ki.dwFlags = KEYEVENTF_KEYUP;
+        SendInput(1, inputs, sizeOf<INPUT>());
+        calloc.free(inputs);
+      }
+    }
+
+    // Free the allocated memory
+    calloc.free(keyboardState);
+
     final keys = hotkeyStrings
         .map((e) => e.toLowerCase())
         .toList();
 
-    await FlutterAutoGUI.hotkey(keys: keys, interval: Duration(milliseconds: 50));
+    await FlutterAutoGUI.hotkey(keys: keys, interval: Duration(milliseconds: 10));
 
     // I've spent so much time debugging this and find out that
     // FlutterAutoGUI.hotkey is a fake future that returns before hotkey
     // is pressed.
-    await Future.delayed(Duration(milliseconds: 60));
+    await Future.delayed(Duration(milliseconds: 50));
   }
 }
