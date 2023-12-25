8c443317d646357db52ef26a9cd1f2a1aeaad890
ryjacky <ryjacky@outlook.com>
2023-12-25 15:52:18 +0900

    chore: add executing state

--- pie_menyu_core/lib/executor/executor_service.dart
+++ pie_menyu_core/lib/executor/executor_service.dart
@@ -3,24 +3,33 @@ import 'package:flutter/material.dart';
 import 'executable.dart';
 
 class ExecutorService extends ChangeNotifier {
-  int _activePieItemOrderIndex = 0;
   final List<Executable> _pieItemTasks = [];
 
+  bool _isExecuting = false;
+  int _activePieItemOrderIndex = 0;
+
+  bool get isExecuting => _isExecuting;
+  
   int get activePieItemOrderIndex => _activePieItemOrderIndex;
 
   set activePieItemOrderIndex(int index) {
     _activePieItemOrderIndex = index;
-    notifyListeners();
   }
 
   void execute(Executable task) {
     _pieItemTasks.add(task);
   }
 
-  void start() {
+  void start() async {
+    _isExecuting = true;
+    notifyListeners();
+
     while (_pieItemTasks.isNotEmpty) {
-      _pieItemTasks.removeAt(0).execute();
+      await _pieItemTasks.removeAt(0).execute();
     }
+
+    _isExecuting = false;
+    notifyListeners();
   }
 
 }
