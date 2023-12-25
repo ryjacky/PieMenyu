25dd02cff748872015cb413478c418dc26df8bb9
ryjacky <ryjacky@outlook.com>
2023-12-25 16:30:04 +0900

    chore: removed redundant setter and getter

--- pie_menyu_core/lib/executor/executor_service.dart
+++ pie_menyu_core/lib/executor/executor_service.dart
@@ -6,15 +6,10 @@ class ExecutorService extends ChangeNotifier {
   final List<Executable> _pieItemTasks = [];
 
   bool _isExecuting = false;
-  int _activePieItemOrderIndex = 0;
+  /// Should not be access through a provider
+  int activePieItemOrderIndex = 0;
 
   bool get isExecuting => _isExecuting;
-  
-  int get activePieItemOrderIndex => _activePieItemOrderIndex;
-
-  set activePieItemOrderIndex(int index) {
-    _activePieItemOrderIndex = index;
-  }
 
   void execute(Executable task) {
     _pieItemTasks.add(task);
