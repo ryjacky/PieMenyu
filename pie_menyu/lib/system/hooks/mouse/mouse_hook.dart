49e0b3071528cd8fb2dffb19007632968f61833d
ryjacky <ryjacky@outlook.com>
2024-01-13 14:59:51 +0900

    chore: MouseHookIsolate now extends SystemHookIsolate and MouseHook now extends SystemHook

--- pie_menyu/lib/system/hooks/mouse/mouse_hook.dart
+++ pie_menyu/lib/system/hooks/mouse/mouse_hook.dart
@@ -4,74 +4,72 @@ import 'dart:ffi';
 import 'dart:isolate';
 
 import 'package:ffi/ffi.dart';
+import 'package:pie_menyu/system/hooks/system_hook.dart';
+import 'package:pie_menyu/system/hooks/system_hook_isolate.dart';
 import 'package:win32/win32.dart';
 
-class MouseHookIsolate {
-  static MouseHookIsolate? _mouseHookIsolate;
+class MouseHookIsolate extends SystemHookIsolate {
+  static MouseHookIsolate? _instance;
 
-  int _hookHandle = NULL;
-  Isolate? _isolate;
   final List<Function(int, int)> _mouseMoveListeners = [];
 
-  factory MouseHookIsolate() {
-    if (_mouseHookIsolate == null) {
-      _mouseHookIsolate = MouseHookIsolate._();
-      _mouseHookIsolate!._startIsolated();
-    }
-    return _mouseHookIsolate!;
-  }
-
   MouseHookIsolate._();
+
+  factory MouseHookIsolate() {
+    if (_instance == null) {
+      _instance = MouseHookIsolate._();
+      _instance!.startIsolated(MouseHook());
+    }
+    return _instance!;
+  }
 
   addMouseMoveListener(Function(int, int) listener) {
     _mouseMoveListeners.add(listener);
   }
 
-  Future<void> _startIsolated() async {
-    final receivePort = ReceivePort();
-
-    _isolate = await Isolate.spawn((sendPort) => MouseHook._(sendPort), receivePort.sendPort);
-    receivePort.listen((message) {
-      if (message is int){
-        _hookHandle = message;
-      }
-      else if (message is String){
-        try {
-          final pos = message.trim().split("\t");
-          final x = int.parse(pos[0]);
-          final y = int.parse(pos[1]);
-          for (var listener in _mouseMoveListeners) {
-            listener(x, y);
-          }
-        } catch (e) {
-          log("Invalid mouse hook info: $message");
+  @override
+  void onMessage(message) {
+    if (message is String){
+      try {
+        final pos = message.trim().split("\t");
+        final x = int.parse(pos[0]);
+        final y = int.parse(pos[1]);
+        for (var listener in _mouseMoveListeners) {
+          listener(x, y);
         }
+      } catch (e) {
+        log("Invalid mouse hook info: $message");
       }
-    });
-
-  }
-
-  void stop(){
-    if (_mouseHookIsolate != null){
-      UnhookWindowsHookEx(_mouseHookIsolate!._hookHandle);
-      _mouseHookIsolate!._isolate?.kill(priority: Isolate.immediate);
-      _mouseHookIsolate = null;
     }
   }
 }
 
-class MouseHook {
-  final SendPort _sendPort;
+class MouseHook extends SystemHook {
+  SendPort? _sendPort;
   int _hookHandle = NULL;
 
-  MouseHook._(this._sendPort) {
+  MouseHook();
+
+  int mouseHookProc(int nCode, int wParam, int lParam) {
+    final pMouseStruct = Pointer<MOUSEHOOKSTRUCT>.fromAddress(lParam);
+    if (nCode >= 0 && wParam == WM_MOUSEMOVE) {
+      _sendPort?.send("${pMouseStruct.ref.pt.x}\t${pMouseStruct.ref.pt.y}");
+    }
+
+    return CallNextHookEx(_hookHandle, nCode, wParam, lParam);
+  }
+
+  @override
+  void start(SendPort sendPort) {
+    _sendPort = sendPort;
+
     final callback = NativeCallable<CallWndProc>.isolateLocal(mouseHookProc,
         exceptionalReturn: 0);
 
     int hInst = GetModuleHandle(nullptr);
     _hookHandle =
         SetWindowsHookEx(WH_MOUSE_LL, callback.nativeFunction, hInst, NULL);
-    _sendPort.send(_hookHandle);
+    _sendPort?.send(_hookHandle);
 
     final msg = calloc<MSG>();
 
@@ -81,14 +79,5 @@ class MouseHook {
     }
 
     calloc.free(msg);
-  }
-
-  int mouseHookProc(int nCode, int wParam, int lParam) {
-    final pMouseStruct = Pointer<MOUSEHOOKSTRUCT>.fromAddress(lParam);
-    if (nCode >= 0 && wParam == WM_MOUSEMOVE) {
-      _sendPort.send("${pMouseStruct.ref.pt.x}\t${pMouseStruct.ref.pt.y}");
-    }
-
-    return CallNextHookEx(_hookHandle, nCode, wParam, lParam);
   }
 }
