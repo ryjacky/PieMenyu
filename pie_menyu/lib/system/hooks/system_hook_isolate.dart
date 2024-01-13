a355a126c3467200c2d104cb8379137e912e16db
ryjacky <ryjacky@outlook.com>
2024-01-13 14:56:11 +0900

    chore: remove instance data member

--- pie_menyu/lib/system/hooks/system_hook_isolate.dart
+++ pie_menyu/lib/system/hooks/system_hook_isolate.dart
@@ -1,21 +1,19 @@
-import 'dart:developer';
 import 'dart:isolate';
 
 import 'package:pie_menyu/system/hooks/system_hook.dart';
 import 'package:win32/win32.dart';
 
 abstract class SystemHookIsolate {
-  static SystemHookIsolate? instance;
-
   int _hookHandle = NULL;
   Isolate? _isolate;
 
   Future<void> startIsolated(SystemHook hook) async {
     final receivePort = ReceivePort();
 
-    _isolate = await Isolate.spawn((sendPort) => hook.start(sendPort), receivePort.sendPort);
+    _isolate = await Isolate.spawn(
+        (sendPort) => hook.start(sendPort), receivePort.sendPort);
     receivePort.listen((message) {
-      if (message is int && _hookHandle == NULL){
+      if (message is int && _hookHandle == NULL) {
         _hookHandle = message;
       } else {
         onMessage(message);
@@ -25,11 +23,8 @@ abstract class SystemHookIsolate {
 
   void onMessage(dynamic message);
 
-  void stop(){
-    if (_instance != null){
-      UnhookWindowsHookEx(_instance!._hookHandle);
-      _instance!._isolate?.kill(priority: Isolate.immediate);
-      _instance = null;
-    }
+  void stop() {
+    UnhookWindowsHookEx(_hookHandle);
+    _isolate?.kill(priority: Isolate.immediate);
   }
 }
