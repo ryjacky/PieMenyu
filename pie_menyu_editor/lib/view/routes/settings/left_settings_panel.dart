2ff2b163527b74ac278e01e26a96c0115adbf28e
ryjacky <ryjacky@outlook.com>
2024-01-16 13:54:04 +0900

    fix: table of content in settings page cannot be selected

--- pie_menyu_editor/lib/view/routes/settings/left_settings_panel.dart
+++ pie_menyu_editor/lib/view/routes/settings/left_settings_panel.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:localization/localization.dart';
 import 'package:pie_menyu_editor/view/widgets/back_icon_button.dart';
 import 'package:pie_menyu_editor/view/widgets/selectable_toc_item.dart';
+import 'package:provider/provider.dart';
+
+import 'settings_state.dart';
 
 class LeftSettingsPanel extends StatefulWidget {
   const LeftSettingsPanel({super.key});
@@ -10,34 +14,35 @@ class LeftSettingsPanel extends StatefulWidget {
 }
 
 class _LeftSettingsPanelState extends State<LeftSettingsPanel> {
-  int _selectedIndex = 0;
-
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    final settingsState = context.read<SettingsState>();
+    final selectedSection = context.select<SettingsState, SettingsSection>(
+        (value) => value.selectedSection);
+
+    return Container(
+      color: Theme.of(context).colorScheme.surface,
       padding: const EdgeInsets.all(18.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // Custom back button
-          const BackIconButton(),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(10, 35, 0, 10),
+            child: BackIconButton(),
+          ),
           // Table of contents
           SelectableTOCItem(
-            title: 'Global settings',
-            onTap: () {
-              setState(() {
-                _selectedIndex = 0;
-              });
-            }, isSelected: true,
+            title: 'label-general-settings'.i18n(),
+            onTap: () =>
+                settingsState.selectedSection = SettingsSection.general,
+            isSelected: selectedSection == SettingsSection.general,
           ),
 
           SelectableTOCItem(
-            title: 'About',
-            onTap: () {
-              setState(() {
-                _selectedIndex = 1;
-              });
-            }, isSelected: false,
+            title: 'label-about'.i18n(),
+            onTap: () => settingsState.selectedSection = SettingsSection.about,
+            isSelected: selectedSection == SettingsSection.about,
           ),
         ],
       ),
--- pie_menyu_editor/lib/view/routes/settings/settings_state.dart
+++ pie_menyu_editor/lib/view/routes/settings/settings_state.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/cupertino.dart';
 
-class SettingsState extends ChangeNotifier {
+enum SettingsSection {
+  general,
+  about,
+}
 
+class SettingsState extends ChangeNotifier {
+  SettingsSection _selectedSection = SettingsSection.general;
+
+  SettingsSection get selectedSection => _selectedSection;
+  set selectedSection(SettingsSection section) {
+    _selectedSection = section;
+    notifyListeners();
+  }
 }
