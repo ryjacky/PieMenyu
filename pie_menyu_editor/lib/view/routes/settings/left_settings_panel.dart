7a08ad9585c876a3def17604b76f92794f1c264e
ryjacky <ryjacky@outlook.com>
2024-01-20 18:46:28 +0900

    refactor: simplifying widget tree of clickable_text.dart (renamed from selectable_toc_item.dart)

--- pie_menyu_editor/lib/view/routes/settings/left_settings_panel.dart
+++ pie_menyu_editor/lib/view/routes/settings/left_settings_panel.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:localization/localization.dart';
 import 'package:pie_menyu_editor/view/widgets/back_icon_button.dart';
-import 'package:pie_menyu_editor/view/widgets/selectable_toc_item.dart';
+import 'package:pie_menyu_editor/view/widgets/clickable_text.dart';
 import 'package:provider/provider.dart';
 
 import 'settings_state.dart';
@@ -14,6 +15,8 @@ class LeftSettingsPanel extends StatefulWidget {
 }
 
 class _LeftSettingsPanelState extends State<LeftSettingsPanel> {
+  final gap = 16.0;
+
   @override
   Widget build(BuildContext context) {
     final settingsState = context.read<SettingsState>();
@@ -22,24 +25,23 @@ class _LeftSettingsPanelState extends State<LeftSettingsPanel> {
 
     return Container(
       color: Theme.of(context).colorScheme.surface,
-      padding: const EdgeInsets.all(18.0),
+      padding: const EdgeInsets.all(32.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // Custom back button
-          const Padding(
-            padding: EdgeInsets.fromLTRB(10, 35, 0, 10),
-            child: BackIconButton(),
-          ),
+          const BackIconButton(),
+          Gap(gap),
           // Table of contents
-          SelectableTOCItem(
+          ClickableText(
             title: 'label-general-settings'.i18n(),
             onTap: () =>
                 settingsState.selectedSection = SettingsSection.general,
             isSelected: selectedSection == SettingsSection.general,
           ),
 
-          SelectableTOCItem(
+          Gap(gap),
+          ClickableText(
             title: 'label-about'.i18n(),
             onTap: () => settingsState.selectedSection = SettingsSection.about,
             isSelected: selectedSection == SettingsSection.about,
---
+++ pie_menyu_editor/lib/view/widgets/clickable_text.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ClickableText extends StatelessWidget {
+  final String title;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  const ClickableText({
+    required this.title,
+    required this.isSelected,
+    required this.onTap,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Text(
+        title,
+        style: TextStyle(
+          fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+          color: isSelected
+              ? Theme.of(context).colorScheme.primary
+              : Theme.of(context).colorScheme.onSurface,
+        ),
+      ),
+    );
+  }
+}
