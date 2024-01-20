42d937e18b98249f4e9d0b7717f99f1737a8c7a9
ryjacky <ryjacky@outlook.com>
2024-01-20 17:08:47 +0900

    refactor: added key param to settingListTile

--- pie_menyu_editor/lib/view/widgets/setting_list_tile.dart
+++ pie_menyu_editor/lib/view/widgets/setting_list_tile.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 
 class SettingListTile extends ListTile {
-  SettingListTile({super.key,
+  SettingListTile({
+    super.key,
     required String title,
     required String subtitle,
-    required Widget trailing,
-    Color? tileColor,
+    required Widget super.trailing,
+    super.tileColor,
   }) : super(
           shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(10)),
           ),
           title: Text(title),
           subtitle: Text(subtitle),
-          tileColor: tileColor,
-          trailing: trailing,
         );
 }
