71d379a49aa1e0e57218ff4f142a8f4438ea21e5
ryjacky <ryjacky@outlook.com>
2024-01-20 22:56:26 +0900

    fix: slow pie item reorder due to large amount of widget redraw in pie_item_list_tab.dart

---
+++ pie_menyu_editor/lib/view/routes/pie_menu_editor/editor_panel/pie_item_list_item.dart
@@ -0,0 +1,139 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gap/gap.dart';
+import 'package:localization/localization.dart';
+import 'package:pie_menyu_core/db/pie_item.dart';
+import 'package:pie_menyu_editor/coreExtended/real_pie_item_instance.dart';
+import 'package:pie_menyu_editor/system/file_icon.dart';
+import 'package:pie_menyu_editor/view/widgets/minimal_text_field.dart';
+import 'package:pie_menyu_editor/view/widgets/single_key_recorder.dart';
+import 'package:provider/provider.dart';
+
+import '../pie_menu_state.dart';
+
+class PieItemListItem extends StatefulWidget {
+  final RealPieItemInstance piInstance;
+  final PieMenuState pieMenuState;
+  const PieItemListItem({super.key, required this.piInstance, required this.pieMenuState});
+
+  @override
+  State<PieItemListItem> createState() => _PieItemListItemState();
+}
+
+class _PieItemListItemState extends State<PieItemListItem> {
+  Image? _icon;
+
+  @override
+  Widget build(BuildContext context) {
+    final pieMenuState = widget.pieMenuState;
+    final piInstance = widget.piInstance;
+
+    _icon ??= createIconWidget(piInstance.pieItem.iconBase64);
+
+    return Row(
+      children: [
+        const Gap(6),
+        createAddIconButton(piInstance.pieItem),
+        const Gap(6),
+        Expanded(
+          child: MinimalTextField(
+            content: piInstance.pieItem.displayName,
+            onSubmitted: (String value) {
+              pieMenuState
+                  .updatePieItem(piInstance.pieItem..displayName = value);
+            },
+          ),
+        ),
+        const Gap(6),
+        SizedBox(
+          width: 32,
+          child: Tooltip(
+            message: "tooltip-pie-item-key".i18n(),
+            child: SingleKeyRecorder(
+              initialValue: piInstance.keyCode,
+              onSubmitted: (String value) {
+                pieMenuState.updatePieItemInstance(piInstance..keyCode = value);
+              },
+            ),
+          ),
+        ),
+        const Gap(6),
+        createDeleteButton(piInstance.pieItem),
+      ],
+    );
+  }
+
+  Widget createAddIconButton(PieItem pieItem) {
+    final themeColorScheme = Theme.of(context).colorScheme;
+    return TextButton(
+      style: TextButton.styleFrom(
+        foregroundColor: themeColorScheme.secondary,
+        padding: const EdgeInsets.all(0),
+        minimumSize: const Size(45, 45),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(3)),
+        side: BorderSide(color: themeColorScheme.outlineVariant, width: 2),
+        backgroundColor: themeColorScheme.surface,
+      ),
+      onPressed: () async {
+        String? icon;
+
+        FilePickerResult? result = await FilePicker.platform.pickFiles();
+        if (result != null) {
+          icon = await FileIcon.getBase64(result.files.single.path!);
+        }
+
+        if (icon != null && context.mounted) {
+          createIconWidget(icon);
+          context.read<PieMenuState>().putPieItem(pieItem..iconBase64 = icon);
+        }
+      },
+      child: _icon!,
+    );
+  }
+
+  Widget createDeleteButton(PieItem pieItem) {
+    return Tooltip(
+      message: "tooltip-long-press-to-delete".i18n(),
+      child: TextButton(
+        style: TextButton.styleFrom(
+          foregroundColor: Colors.red,
+          padding: const EdgeInsets.all(5),
+          minimumSize: const Size(36, 36),
+        ),
+        onPressed: () {},
+        child: const Icon(FontAwesomeIcons.minus, color: Colors.red, size: 12),
+        onLongPress: () {
+          final result = context.read<PieMenuState>().removePieItem(pieItem);
+          final scaffoldMessenger = ScaffoldMessenger.of(context);
+          if (!result) {
+            scaffoldMessenger.hideCurrentSnackBar();
+            scaffoldMessenger.showSnackBar(
+              SnackBar(
+                backgroundColor: Colors.red[400],
+                content: Text(
+                  "message-pie-item-not-deleted-${Random().nextInt(5)}".i18n(),
+                ),
+                duration: const Duration(seconds: 5),
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  createIconWidget(String iconBase64) {
+    return Image.memory(
+      width: 28,
+      height: 28,
+      isAntiAlias: true,
+      base64Decode(iconBase64),
+      errorBuilder: (_, __, ___) => const Icon(Icons.upload_file),
+    );
+  }
+}
+
--- pie_menyu_editor/lib/view/routes/pie_menu_editor/editor_panel/pie_item_list_tab.dart
+++ pie_menyu_editor/lib/view/routes/pie_menu_editor/editor_panel/pie_item_list_tab.dart
@@ -1,19 +1,9 @@
-import 'dart:convert';
-import 'dart:math';
-
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:gap/gap.dart';
 import 'package:localization/localization.dart';
 import 'package:pie_menyu_core/db/pie_item.dart';
-import 'package:pie_menyu_core/db/pie_menu.dart';
-import 'package:pie_menyu_editor/coreExtended/real_pie_item_instance.dart';
-import 'package:pie_menyu_editor/system/file_icon.dart';
+import 'package:pie_menyu_editor/view/routes/pie_menu_editor/editor_panel/pie_item_list_item.dart';
 import 'package:pie_menyu_editor/view/widgets/primary_button.dart';
-import 'package:pie_menyu_editor/view/widgets/single_key_recorder.dart';
-import 'package:pie_menyu_editor/view/widgets/minimal_text_field.dart';
 import 'package:provider/provider.dart';
 
 import '../pie_menu_state.dart';
@@ -26,14 +16,14 @@ class PieItemListTab extends StatefulWidget {
 }
 
 class _PieItemListTabState extends State<PieItemListTab> {
-  static const double gap = 6;
+  Widget? icon;
 
   @override
   Widget build(BuildContext context) {
     final pieItemInstances = context.watch<PieMenuState>().pieItemInstances;
     final pieMenuState = context.watch<PieMenuState>();
 
-    return ReorderableListView.builder(
+    return ReorderableListView(
       buildDefaultDragHandles: false,
       header: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -44,122 +34,35 @@ class _PieItemListTabState extends State<PieItemListTab> {
           icon: FontAwesomeIcons.plus,
         ),
       ),
-      itemCount: pieItemInstances.length,
       onReorder: (oldI, newI) => pieMenuState.reorderPieItem(oldI, newI - 1),
-      itemBuilder: (BuildContext context, int index) {
-        final piInstance = pieItemInstances[index];
-        return Padding(
-          key: ValueKey(piInstance),
-          padding: const EdgeInsets.symmetric(horizontal: 6.0, vertical: 4),
-          child: Row(
-            children: [
-              ReorderableDragStartListener(
-                index: index,
-                child: MouseRegion(
-                  cursor: SystemMouseCursors.resizeDown,
-                  child: Icon(
-                    FontAwesomeIcons.gripVertical,
-                    size: 15,
-                    color: Theme.of(context).colorScheme.secondary,
+      children: [
+        for (final piInstance in pieItemInstances)
+          Padding(
+            key: ValueKey(piInstance.pieItemId),
+            padding: const EdgeInsets.symmetric(horizontal: 6.0, vertical: 4),
+            child: Row(
+              children: [
+                ReorderableDragStartListener(
+                  index: pieItemInstances.indexOf(piInstance),
+                  child: MouseRegion(
+                    cursor: SystemMouseCursors.resizeDown,
+                    child: Icon(
+                      FontAwesomeIcons.gripVertical,
+                      size: 15,
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
                   ),
                 ),
-              ),
-              const Gap(gap),
-              createAddIconButton(piInstance.pieItem),
-              const Gap(gap),
-              Expanded(
-                child: MinimalTextField(
-                  content: piInstance.pieItem.displayName,
-                  onSubmitted: (String value) {
-                    pieMenuState
-                        .updatePieItem(piInstance.pieItem..displayName = value);
-                  },
-                ),
-              ),
-              const Gap(gap),
-              SizedBox(
-                width: 32,
-                child: Tooltip(
-                  message: "tooltip-pie-item-key".i18n(),
-                  child: SingleKeyRecorder(
-                    initialValue: piInstance.keyCode,
-                    onSubmitted: (String value) {
-                      pieMenuState
-                          .updatePieItemInstance(piInstance..keyCode = value);
-                    },
+                Expanded(
+                  child: PieItemListItem(
+                    piInstance: piInstance,
+                    pieMenuState: pieMenuState,
                   ),
-                ),
-              ),
-              const Gap(gap),
-              createDeleteButton(piInstance.pieItem),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-  Widget createAddIconButton(PieItem pieItem) {
-    final themeColorScheme = Theme.of(context).colorScheme;
-    return TextButton(
-      style: TextButton.styleFrom(
-        foregroundColor: themeColorScheme.secondary,
-        padding: const EdgeInsets.all(0),
-        minimumSize: const Size(45, 45),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(3)),
-        side: BorderSide(color: themeColorScheme.outlineVariant, width: 2),
-        backgroundColor: themeColorScheme.surface,
-      ),
-      onPressed: () async {
-        String? icon;
-
-        FilePickerResult? result = await FilePicker.platform.pickFiles();
-        if (result != null) {
-          icon = await FileIcon.getBase64(result.files.single.path!);
-        }
-
-        if (icon != null && context.mounted) {
-          context.read<PieMenuState>().putPieItem(pieItem..iconBase64 = icon);
-        }
-      },
-      child: Image.memory(
-        width: 28,
-        height: 28,
-        isAntiAlias: true,
-        base64Decode(pieItem.iconBase64),
-        errorBuilder: (_, __, ___) => const Icon(Icons.upload_file),
-      ),
-    );
-  }
-
-  Widget createDeleteButton(PieItem pieItem) {
-    return Tooltip(
-      message: "tooltip-long-press-to-delete".i18n(),
-      child: TextButton(
-        style: TextButton.styleFrom(
-          foregroundColor: Colors.red,
-          padding: const EdgeInsets.all(5),
-          minimumSize: const Size(36, 36),
-        ),
-        onPressed: () {},
-        child: const Icon(FontAwesomeIcons.minus, color: Colors.red, size: 12),
-        onLongPress: () {
-          final result = context.read<PieMenuState>().removePieItem(pieItem);
-          final scaffoldMessenger = ScaffoldMessenger.of(context);
-          if (!result) {
-            scaffoldMessenger.hideCurrentSnackBar();
-            scaffoldMessenger.showSnackBar(
-              SnackBar(
-                backgroundColor: Colors.red[400],
-                content: Text(
-                  "message-pie-item-not-deleted-${Random().nextInt(5)}".i18n(),
-                ),
-                duration: const Duration(seconds: 5),
-              ),
-            );
-          }
-        },
-      ),
+                )
+              ],
+            ),
+          )
+      ],
     );
   }
 }
