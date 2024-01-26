4fa6fc83f2488c0e92ed3aa76307a34f17931f73
ryjacky <ryjacky@outlook.com>
2024-01-26 20:06:23 +0900

    refactor: moving deep link registration to installer file

--- pie_menyu/lib/deep_linking/deep_link_handler.dart
+++ pie_menyu/lib/deep_linking/deep_link_handler.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:app_links/app_links.dart';
 import 'package:flutter/material.dart';
 import 'package:system_tray/system_tray.dart';
-import 'package:win32_registry/win32_registry.dart';
 import 'package:window_manager/window_manager.dart';
 
 enum DeepLinkCommand {
@@ -20,8 +19,6 @@ class DeepLinkHandler {
   }
 
   DeepLinkHandler() {
-    register('piemenyu');
-
     final appLinks = AppLinks();
 
     appLinks.allUriLinkStream.listen((uri) async {
@@ -42,26 +39,5 @@ class DeepLinkHandler {
       await AppWindow().hide();
 
     });
-  }
-
-  Future<void> register(String scheme) async {
-    String appPath = Platform.resolvedExecutable;
-
-    String protocolRegKey = 'Software\\Classes\\$scheme';
-    RegistryValue protocolRegValue = const RegistryValue(
-      'URL Protocol',
-      RegistryValueType.string,
-      '',
-    );
-    String protocolCmdRegKey = 'shell\\open\\command';
-    RegistryValue protocolCmdRegValue = RegistryValue(
-      '',
-      RegistryValueType.string,
-      '"$appPath" "%1"',
-    );
-
-    final regKey = Registry.currentUser.createKey(protocolRegKey);
-    regKey.createValue(protocolRegValue);
-    regKey.createKey(protocolCmdRegKey).createValue(protocolCmdRegValue);
   }
 }
