b1eef51b74d9ef5220dc91aed94cab8d3a141cc0
ryjacky127 <ryjacky127@gmail.com>
2024-04-14 18:15:29 +0900

    fix: file type icon instead of thumbnail is extracted in pie_item_list_item.dart

--- pie_menyu_editor/lib/view/routes/pie_menu_editor/editor_panel/pie_item_list_item.dart
+++ pie_menyu_editor/lib/view/routes/pie_menu_editor/editor_panel/pie_item_list_item.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'dart:math';
 
 import 'package:file_picker/file_picker.dart';
@@ -6,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gap/gap.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:mime/mime.dart';
 import 'package:pie_menyu_core/db/pie_item.dart';
 import 'package:pie_menyu_core/db/pie_menu.dart';
 import 'package:pie_menyu_core/widgets/pieMenuView/pie_menu_state.dart';
@@ -99,9 +101,15 @@ class _PieItemListItemState extends State<PieItemListItem> {
       onPressed: () async {
         String? icon;
 
-        FilePickerResult? result = await FilePicker.platform.pickFiles();
-        if (result != null) {
-          icon = await FileIcon.getBase64(result.files.single.path!);
+        PlatformFile? result = (await FilePicker.platform.pickFiles())?.files.firstOrNull;
+        if (result != null && result.path != null) {
+          String mimeType = lookupMimeType(result.path!) ?? "";
+
+          if (mimeType.startsWith("image") && !result.path!.contains("svg")){
+            icon = base64Encode(File(result.path!).readAsBytesSync());
+          } else {
+            icon = await FileIcon.getBase64(result.path!);
+          }
         }
 
         if (icon != null && context.mounted) {
