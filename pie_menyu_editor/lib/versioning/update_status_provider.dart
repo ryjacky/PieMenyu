370022883d010223d3525d63aa2e3cdb7f2b56bf
ryjacky127 <ryjacky127@gmail.com>
2024-05-19 22:34:56 +0900

    Updated update_status_provider.dart to remove dependency on winget

--- pie_menyu_editor/lib/versioning/update_status_provider.dart
+++ pie_menyu_editor/lib/versioning/update_status_provider.dart
@@ -1,9 +1,14 @@
 import 'dart:io';
 
+import 'package:background_downloader/background_downloader.dart';
 import 'package:flutter/material.dart';
-import 'package:process_run/process_run.dart';
+import 'package:http/http.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:path/path.dart' as path;
 
 class UpdateStatusProvider extends ChangeNotifier {
+  final String filename = "PieMenyuInstaller.exe";
+
   bool _updating = false;
   bool _updateAvailable = false;
 
@@ -22,24 +27,46 @@ class UpdateStatusProvider extends ChangeNotifier {
   }
 
   void fetchUpdate() async {
-    // Check for update
-    ProcessResult? result;
-    if (Platform.isWindows) {
-      result = (await Shell().run("winget upgrade")).firstOrNull;
-    }
+    final rawVersion = await Future.wait([
+      get(Uri.parse(
+          "https://github.com/ryjacky/PieMenyu/releases/download/versionInfo/latest")),
+      PackageInfo.fromPlatform()
+    ]);
 
-    if (result != null && result.outText.contains("PieMenyu")) {
+    int rawUpdateAvailable = ((rawVersion[0] as Response)
+        .body
+        .compareTo((rawVersion[1] as PackageInfo).version));
+
+    if (rawUpdateAvailable > 0) {
       updateAvailable = true;
+    } else {
+      updateAvailable = false;
     }
-
-    updateAvailable = false;
   }
 
   Future<void> update() async {
     if (Platform.isWindows) {
       updating = true;
-      await Process.start("winget", ["upgrade", "PieMenyu"],
-          mode: ProcessStartMode.detached);
+
+      final file =
+          File(path.join(Directory.systemTemp.path, filename));
+      if (await file.exists()) await file.delete();
+
+      final task = DownloadTask(
+        url:
+            'https://github.com/ryjacky/PieMenyu/releases/latest/download/$filename',
+        filename: filename,
+        directory: Directory.systemTemp.path,
+        updates: Updates.statusAndProgress,
+        retries: 3,
+      );
+
+      final result = await FileDownloader().download(task, onStatus: (status) {
+        if (status == TaskStatus.complete) {
+          updating = false;
+          Process.run(file.path, []);
+        }
+      });
     }
   }
 }
