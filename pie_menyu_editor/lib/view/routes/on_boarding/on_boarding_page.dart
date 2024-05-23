6455a754eb4a01129f9780018bb94719445ba565
ryjacky127 <ryjacky127@gmail.com>
2024-05-23 13:23:12 +0900

    Rewritten onboarding pages to clearly describe the purpose of PieMenyu

--- pie_menyu_editor/lib/view/routes/on_boarding/on_boarding_page.dart
+++ pie_menyu_editor/lib/view/routes/on_boarding/on_boarding_page.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import 'package:pie_menyu_editor/preferences/editor_preferences.dart';
 import 'package:pie_menyu_editor/view/routes/home/home_route.dart';
@@ -55,31 +56,9 @@ class OnBoardingPageState extends State<OnBoardingPage> {
             allowImplicitScrolling: true,
             infiniteAutoScroll: false,
             pages: [
-              PageViewModel(
-                title: "label-welcome".tr(),
-                body: "description-welcome".tr(),
-                image: _buildImage('icon.png'),
-                decoration: pageDecoration,
-              ),
-              PageViewModel(
-                title: "label-q-language".tr(),
-                bodyWidget: LanguageDropdownMenu(
-                  initialSelection: context.locale.languageCode,
-                  onSelected: (value) {
-                    if (value == null) return;
-                    context.setLocale(Locale(value));
-                  },
-                ),
-                image: SvgPicture.asset(
-                  "assets/images/language-solid.svg",
-                  width: 150,
-                  colorFilter: const ColorFilter.mode(
-                    Colors.white70,
-                    BlendMode.srcIn,
-                  ),
-                ),
-                decoration: pageDecoration,
-              ),
+              createPage1(pageDecoration),
+              createPage2(pageDecoration),
+              createPage3(pageDecoration),
             ],
             onDone: () => _onIntroEnd(context),
             onSkip: () => _onIntroEnd(context),
@@ -113,6 +92,102 @@ class OnBoardingPageState extends State<OnBoardingPage> {
           ),
         ),
       ],
+    );
+  }
+
+  PageViewModel createPage1(PageDecoration pageDecoration) {
+    return PageViewModel(
+      title: "label-welcome".tr(),
+      bodyWidget: Column(
+        children: [
+          Text(
+            "description-welcome".tr(),
+            style: const TextStyle(color: Colors.grey),
+          ),
+          const Gap(15),
+          LanguageDropdownMenu(
+            initialSelection: context.locale.languageCode,
+            onSelected: (value) {
+              if (value == null) return;
+              context.setLocale(Locale(value));
+            },
+          ),
+        ],
+      ),
+      image: _buildImage('icon.png'),
+      decoration: pageDecoration,
+    );
+  }
+
+  PageViewModel createPage2(PageDecoration pageDecoration) {
+    const TextStyle bodyStyle = TextStyle(color: Colors.grey);
+    return PageViewModel(
+      title: "label-where-to-use-pie-menyu".tr(),
+      bodyWidget: GridView.count(
+        crossAxisCount: 5,
+        mainAxisSpacing: 15,
+        padding: const EdgeInsets.only(top: 35),
+        shrinkWrap: true,
+        children: [
+          SvgPicture.asset(
+            'assets/images/why-pie-menyu-1.svg',
+            colorFilter: ColorFilter.mode(
+              Theme.of(context).colorScheme.primary,
+              BlendMode.srcIn,
+            ),
+          ),
+          const VerticalDivider(),
+          SvgPicture.asset(
+            'assets/images/why-pie-menyu-2.svg',
+            colorFilter: ColorFilter.mode(
+              Theme.of(context).colorScheme.primary,
+              BlendMode.srcIn,
+            ),
+          ),
+          const VerticalDivider(),
+          SvgPicture.asset(
+            'assets/images/why-pie-menyu-3.svg',
+            colorFilter: ColorFilter.mode(
+              Theme.of(context).colorScheme.primary,
+              BlendMode.srcIn,
+            ),
+          ),
+          Text(
+            "description-why-pie-menyu-1".tr(),
+            style: bodyStyle,
+            textAlign: TextAlign.center,
+          ),
+          const Gap(0),
+          Text(
+            "description-why-pie-menyu-2".tr(),
+            style: bodyStyle,
+            textAlign: TextAlign.center,
+          ),
+          const Gap(0),
+          Text(
+            "description-why-pie-menyu-3".tr(),
+            style: bodyStyle,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+      decoration: pageDecoration,
+    );
+  }
+
+  PageViewModel createPage3(PageDecoration pageDecoration) {
+    return PageViewModel(
+      title: "label-try-pie-menyu".tr(),
+      body: "description-try-pie-menyu".tr(),
+      image: SvgPicture.asset(
+        'assets/images/pie-menu.svg',
+        height: 150,
+        colorFilter: ColorFilter.mode(
+          Theme.of(context).colorScheme.primary,
+          BlendMode.srcIn,
+        ),
+      ),
+      decoration: pageDecoration,
     );
   }
 }
