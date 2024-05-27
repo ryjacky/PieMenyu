e0413a8c8caa204a5bcd3df169bac8e43e7154fe
ryjacky127 <ryjacky127@gmail.com>
2024-05-27 22:40:59 +0900

    Refactored PieItem class to provide a more intuitive programming interface.
    The class now provide a way to distinguish between a base64 icon and a font icon (MaterialIcon) through null checks

--- pie_menyu_core/lib/db/pie_item.dart
+++ pie_menyu_core/lib/db/pie_item.dart
@@ -10,16 +10,40 @@ part 'pie_item.g.dart';
 class PieItem {
   Id id = Isar.autoIncrement;
 
-  String iconBase64 = '';
+  String? _iconBase64;
+
+  String? get iconBase64 => _iconBase64;
+
+  set iconBase64(String? icon) {
+    _iconBase64 = icon;
+    _iconDataCode = null;
+  }
+
+  int? _iconDataCode;
+
+  int? get iconDataCode => _iconDataCode;
+
+  set iconDataCode(int? code) {
+    _iconDataCode = code;
+    _iconBase64 = null;
+  }
+
   String name = '';
 
   List<PieItemTask> tasks = [];
 
-  PieItem({this.iconBase64 = '', this.name = ''});
+  PieItem({String? iconBase64, this.name = '', int? iconDataCode}) {
+    if (iconBase64 != null && iconDataCode != null) throw Exception("Only provide iconBase64 or iconDataCode but neither of them!");
+    _iconBase64 = iconBase64;
+    _iconDataCode = iconDataCode;
+  }
 
   PieItem.from(PieItem pieItem) {
+    if (iconBase64 != null && iconDataCode != null) throw Exception("Only provide iconBase64 or iconDataCode but neither of them!");
+
     id = pieItem.id;
-    iconBase64 = pieItem.iconBase64;
+    _iconDataCode = pieItem.iconDataCode;
+    _iconBase64 = pieItem.iconBase64;
     name = pieItem.name;
     tasks = pieItem.tasks.toList();
   }
