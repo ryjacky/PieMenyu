55741368df4c4489db95ebbc52e33426f72c48c5
ryjacky127 <ryjacky127@gmail.com>
2024-05-27 23:16:54 +0900

    Refactored the upload icon button to show a list of available icons for users to choose when hovered on it.

--- pie_menyu_editor/lib/view/routes/pie_menu_editor/editor_panel/pie_item_list_item.dart
+++ pie_menyu_editor/lib/view/routes/pie_menu_editor/editor_panel/pie_item_list_item.dart
@@ -5,7 +5,6 @@ import 'dart:math';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gap/gap.dart';
 import 'package:mime/mime.dart';
 import 'package:pie_menyu_core/db/pie_item.dart';
@@ -33,7 +32,72 @@ class PieItemListItem extends StatefulWidget {
 }
 
 class _PieItemListItemState extends State<PieItemListItem> {
-  Image? _icon;
+  Widget? _icon;
+
+  static const List<IconData> icons = [
+    Icons.paste,
+    Icons.save,
+    Icons.settings,
+    Icons.search,
+    Icons.share,
+    Icons.star,
+    Icons.brush,
+    Icons.camera,
+    Icons.cloud,
+    Icons.delete,
+    Icons.edit,
+    Icons.email,
+    Icons.favorite,
+    Icons.home,
+    Icons.image,
+    Icons.info,
+    Icons.link,
+    Icons.menu,
+    Icons.notifications,
+    Icons.people,
+    Icons.phone,
+    Icons.print,
+    Icons.imagesearch_roller,
+    Icons.looks_one,
+    Icons.looks_two,
+    Icons.looks_3,
+    Icons.looks_4,
+    Icons.looks_5,
+    Icons.looks_6,
+    Icons.web,
+    Icons.work,
+    Icons.water_drop,
+    Icons.layers,
+    Icons.blur_on,
+    Icons.filter,
+    Icons.pin_end,
+    Icons.format_color_fill,
+    Icons.text_format,
+    Icons.view_in_ar,
+    Icons.select_all,
+    Icons.mouse,
+    Icons.keyboard,
+    Icons.add_a_photo,
+    Icons.fiber_smart_record,
+    Icons.public,
+    Icons.save,
+    Icons.copy,
+    Icons.auto_fix_high,
+    Icons.volume_up,
+    Icons.volume_down,
+    Icons.volume_off,
+    Icons.play_circle,
+    Icons.pause_circle,
+    Icons.stop_circle,
+    Icons.skip_next,
+    Icons.skip_previous,
+    Icons.replay,
+    Icons.animation,
+    Icons.deblur,
+    Icons.folder_copy,
+    Icons.folder_open,
+    Icons.file_open,
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -43,13 +107,25 @@ class _PieItemListItemState extends State<PieItemListItem> {
     final pieMenuState = widget.pieMenuState;
     final piInstance = widget.pieItemDelegate;
 
-    _icon = Image.memory(
-      width: 28,
-      height: 28,
-      isAntiAlias: true,
-      base64Decode(pieItem.iconBase64),
-      errorBuilder: (_, __, ___) => const Icon(Icons.upload_file),
-    );
+    // Check if the icon is an IconData or a base64 string and display it accordingly
+    // If the icon is not set, display a default icon
+    if (pieItem.iconDataCode != null) {
+      _icon = Icon(
+        IconData(pieItem.iconDataCode!, fontFamily: 'MaterialIcons'),
+        size: 20,
+      );
+    } else if (pieItem.iconBase64 != null) {
+      _icon = Image.memory(
+        base64Decode(pieItem.iconBase64!),
+        width: 20,
+        height: 20,
+        errorBuilder: (context, object, error) {
+          return const Icon(Icons.upload_file, size: 25);
+        },
+      );
+    } else {
+      _icon = const Icon(Icons.upload_file, size: 25);
+    }
 
     return Row(
       children: [
@@ -80,7 +156,7 @@ class _PieItemListItemState extends State<PieItemListItem> {
                     .every((pieItem) => pieItem.keyCode != value);
 
                 if (!valid) {
-                  showErrorSnackBar();
+                  showSnackBar("message-slice-key-is-used", Colors.red[400]!);
                 }
                 return valid;
               },
@@ -96,114 +172,149 @@ class _PieItemListItemState extends State<PieItemListItem> {
 
   Widget createAddIconButton(PieItem pieItem) {
     final themeColorScheme = Theme.of(context).colorScheme;
-    return (button) {
-      if (pieItem.iconBase64 == "") return button;
-
-      return Tooltip(
-        message: "tooltip-remove-icon".tr(),
-        child: button,
-        preferBelow: false,
-      );
-    }(TextButton(
-      style: TextButton.styleFrom(
-        foregroundColor: themeColorScheme.secondary,
-        padding: const EdgeInsets.all(0),
-        minimumSize: const Size(45, 45),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(3)),
-        side: BorderSide(color: themeColorScheme.outlineVariant, width: 2),
-        backgroundColor: themeColorScheme.surface,
+    return Tooltip(
+      enableTapToDismiss: false,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: themeColorScheme.surface,
+        border: Border.all(color: themeColorScheme.outlineVariant, width: 1),
       ),
-      onPressed: () async {
-        String? icon;
-
-        if (pieItem.iconBase64 == "") {
-          PlatformFile? result =
-              (await FilePicker.platform.pickFiles())?.files.firstOrNull;
-          if (result != null && result.path != null) {
-            String mimeType = lookupMimeType(result.path!) ?? "";
-
-            if (mimeType.startsWith("image") && !result.path!.contains("svg")) {
-              icon = base64Encode(File(result.path!).readAsBytesSync());
-            } else {
-              icon = await FileIcon.getBase64(result.path!);
-            }
-          }
-        } else {
-          icon = "";
-        }
-
-        if (icon != null && mounted) {
-          context.read<PieMenuState>().putPieItem(pieItem..iconBase64 = icon);
-          setState(() {});
-        }
-      },
-      child: _icon!,
-    ));
-  }
-
-  Widget createDeleteButton(PieItem pieItem) {
-    return TextButton(
-      style: TextButton.styleFrom(
-        foregroundColor: Colors.red,
-        padding: const EdgeInsets.all(0),
-        minimumSize: const Size(36, 36),
+      richMessage: WidgetSpan(
+        child: SizedBox(
+          width: 300,
+          height: 300,
+          child: GridView.count(
+            shrinkWrap: true,
+            crossAxisCount: 8,
+            children: [
+              _buildIconButton(
+                Icons.upload_file,
+                onPressed: () => pickIcon(pieItem),
+              ),
+              _buildIconButton(
+                Icons.remove_circle_outline_rounded,
+                fgColor: Colors.red,
+                onPressed: () => setState(() {
+                  context
+                      .read<PieMenuState>()
+                      .putPieItem(pieItem..iconBase64 = null);
+                }),
+              ),
+              const VerticalDivider(indent: 7, endIndent: 7),
+              for (var icon in icons)
+                _buildIconButton(
+                  icon,
+                  onPressed: () => setState(() {
+                    context
+                        .read<PieMenuState>()
+                        .putPieItem(pieItem..iconDataCode = icon.codePoint);
+                  }),
+                  fgColor: themeColorScheme.secondary,
+                )
+            ],
+          ),
+        ),
       ),
-      child:
-          const Icon(Icons.delete_outline_rounded, color: Colors.red, size: 20),
-      onPressed: () {
-        final pieMenuState = context.read<PieMenuState>();
-        final scaffoldMessenger = ScaffoldMessenger.of(context);
-        if (!pieMenuState.removePieItem(pieItem)) {
-          scaffoldMessenger.hideCurrentSnackBar();
-          scaffoldMessenger.showSnackBar(
-            SnackBar(
-              behavior: SnackBarBehavior.floating,
-              margin: const EdgeInsets.fromLTRB(8, 8, 334, 8),
-              backgroundColor: Colors.red[400],
-              content: Text(
-                "message-pie-item-not-deleted-${Random().nextInt(5)}".tr(),
-              ),
-              duration: const Duration(seconds: 5),
-            ),
-          );
-        } else {
-          final pieMenuState = context.read<PieMenuState>();
-          pieMenuState.activePieItemDelegate =
-              pieMenuState.pieItemDelegates.first;
-
-          scaffoldMessenger.hideCurrentSnackBar();
-          scaffoldMessenger.showSnackBar(
-            SnackBar(
-              behavior: SnackBarBehavior.floating,
-              margin: const EdgeInsets.fromLTRB(8, 8, 334, 8),
-              action: SnackBarAction(
-                label: "label-undo".tr(),
-                onPressed: () {
-                  pieMenuState.undoRemove();
-                  scaffoldMessenger.clearSnackBars();
-                },
-              ),
-              backgroundColor: Theme.of(context).colorScheme.primary,
-              content: Text("message-removed-pie-item".tr()),
-              duration: const Duration(seconds: 5),
-            ),
-          );
-        }
-      },
+      child: TextButton(
+        style: TextButton.styleFrom(
+          foregroundColor: themeColorScheme.secondary,
+          padding: const EdgeInsets.all(0),
+          minimumSize: const Size(45, 45),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(3)),
+          side: BorderSide(color: themeColorScheme.outlineVariant, width: 2),
+          backgroundColor: themeColorScheme.surface,
+        ),
+        onPressed: () => pickIcon(pieItem),
+        child: _icon!,
+      ),
     );
   }
 
-  void showErrorSnackBar() {
+  IconButton _buildIconButton(
+    IconData iconData, {
+    required Function() onPressed,
+    Color? fgColor,
+  }) {
+    return IconButton(
+      icon: Icon(iconData, color: fgColor),
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        foregroundColor: fgColor,
+        padding: const EdgeInsets.all(5),
+        minimumSize: const Size(0, 0),
+      ),
+    );
+  }
+
+  void showSnackBar(String message, Color backgroundColor,
+      {SnackBarAction? action}) {
     final scaffoldMessenger = ScaffoldMessenger.of(context);
     scaffoldMessenger.hideCurrentSnackBar();
     scaffoldMessenger.showSnackBar(
       SnackBar(
         behavior: SnackBarBehavior.floating,
         margin: const EdgeInsets.fromLTRB(8, 8, 334, 8),
-        backgroundColor: Colors.red[400],
-        content: Text("message-slice-key-is-used".tr()),
+        action: action,
+        backgroundColor: backgroundColor,
+        content: Text(message.tr()),
         duration: const Duration(seconds: 5),
       ),
     );
+  }
+
+  Widget createDeleteButton(PieItem pieItem) {
+    return _buildIconButton(
+      Icons.delete_outline_rounded,
+      fgColor: Colors.red,
+      onPressed: () {
+        final pieMenuState = context.read<PieMenuState>();
+        final scaffoldMessenger = ScaffoldMessenger.of(context);
+
+        if (!pieMenuState.removePieItem(pieItem)) {
+          showSnackBar(
+            "message-pie-item-not-deleted-${Random().nextInt(5)}",
+            Colors.red[400]!,
+          );
+        } else {
+          final pieMenuState = context.read<PieMenuState>();
+          pieMenuState.activePieItemDelegate =
+              pieMenuState.pieItemDelegates.first;
+
+          showSnackBar(
+            "message-removed-pie-item",
+            Theme.of(context).colorScheme.primary,
+            action: SnackBarAction(
+              label: "label-undo".tr(),
+              onPressed: () {
+                pieMenuState.undoRemove();
+                scaffoldMessenger.clearSnackBars();
+              },
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  pickIcon(PieItem pieItem) async {
+    String? icon;
+
+    PlatformFile? result =
+        (await FilePicker.platform.pickFiles())?.files.firstOrNull;
+    if (result != null && result.path != null) {
+      String mimeType = lookupMimeType(result.path!) ?? "";
+
+      if (mimeType.startsWith("image") && !result.path!.contains("svg")) {
+        icon = base64Encode(File(result.path!).readAsBytesSync());
+      } else {
+        icon = await FileIcon.getBase64(result.path!);
+      }
+    }
+
+    if (icon != null && mounted) {
+      setState(() {
+        context.read<PieMenuState>().putPieItem(pieItem..iconBase64 = icon);
+      });
+    }
   }
 }
